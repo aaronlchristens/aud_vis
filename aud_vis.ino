e3702b39fdc7761128855a94ff8f9f914a775739
Aaron Christensen <aaronch@umich.edu>
2015-06-18 17:44:25 -0400

    Cleaned up, beginning to implement scroll from midpoint with volume only.

--- aud_vis.ino
+++ aud_vis.ino
@@ -11,6 +11,8 @@ unsigned long currentMicros = 0;
 unsigned long lastRead = 0; //Last time spectrum was read; long because will be operated with currentMicros
 byte band;
 uint8_t volume[7] = { 0, 0, 0, 0, 0, 0, 0};
+uint8_t volumeTotal = 0;
+const uint8_t volumeMax = 1024 / 4; //1024 from each band (* 7) scaled down (/ (4 * 7)) to be one bit
 
 #include <Adafruit_DotStar.h>
 // Because conditional #includes don't work w/Arduino sketches...
@@ -24,7 +26,6 @@ uint8_t volume[7] = { 0, 0, 0, 0, 0, 0, 0};
 #define CLOCKPIN   13
 Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS);//, DATAPIN, CLOCKPIN);
 
-int displayLength = int(NUMPIXELS / 7);
 // Hardware SPI is a little faster, but must be wired to specific pins
 // (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
 //Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS);
@@ -34,7 +35,7 @@ void setup() {
   //clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
 #endif
   strip.begin(); // Initialize pins for output
-  strip.show();  // Turn all LEDs off ASAP
+  strip.show();  // Turn all LEDs off
   startMonitor(); //Begin printing to serial monitor for debugging
   //Setup pins to drive the spectrum analyzer. 
   pinMode(spectrumReset, OUTPUT);
@@ -46,50 +47,43 @@ void loop() {
   currentMicros = micros(); //Start micros, overflows and resets to 0 every 70 minutes
   //fails after 70 minutes? --> no
   if (currentMicros - lastRead >= 100000){ //Read 10 times/second
-    read(); //Reads and updates leds
+    read();
+    updateLeds();
   }
 }
 
-void reset(){
-  digitalWrite(spectrumStrobe,LOW);
-  //delay(1);
-  digitalWrite(spectrumReset,HIGH);
-  //delay(1);
-  digitalWrite(spectrumStrobe,HIGH);
-  //delay(1);
-  digitalWrite(spectrumStrobe,LOW);
-  //delay(1);
-  digitalWrite(spectrumReset,LOW);
-  //delay(5);
-  // Reading the analyzer now will read the lowest frequency.
-}
-
-void cycle(){
-  //Cycle analyzer
-  digitalWrite(spectrumStrobe,HIGH);
-  digitalWrite(spectrumStrobe,LOW);  
-}
-
 void read(){
-  lastRead = currentMicros;
   reset();
+  volumeTotal = 0;
   for(band=0; band <7; band++){
-    volume[band] = (((analogRead(0) + analogRead(1) ) / 2)/4); //Read each channel and average
-    //analog reads in voltage 0-5V or 0-1023, so divide by four to scale to one byte (0-255)
+    volume[band] = (((analogRead(0) + analogRead(1) ) / 2)/28); //Read each channel and average
+    //analog reads in voltage 0-5V or 0-1023, so divide by 4*7 to scale to 1/7 of a byte
     if (volume[band] < 20){
       volume[band] = 0;
     }
     //printBand(band);   
-    updateLeds(band);
+    volumeTotal += volume[band];
     cycle();
   }
+  lastRead = currentMicros;
   //Serial.println(); 
+}
+
+void updateLeds(){
+    for (int i = head; i < NUMPIXELS; i++){
+        strip.setPixelColor(i, volumeTotal/2, volumeTotal/2, volumeTotal/2);
+    }
+    strip.show();
+}
+
+void cycle(){
+    //Cycle analyzer
+    digitalWrite(spectrumStrobe, HIGH);
+    digitalWrite(spectrumStrobe, LOW);
 }
 
 void startMonitor(){
   Serial.begin(115200);
-  //Serial.write("Monitor started.");
-  //Serial.println();
 }
 
 void printBand(int band){
@@ -97,34 +91,16 @@ void printBand(int band){
   Serial.print(",");
 }
 
-void updateLeds(int band){
-  head = band * displayLength;
-  for (int i = head; i < head + (displayLength - 1); i++){
-    switch(band){
-      case 0:
-        strip.setPixelColor(i, volume[band], 0,0);
-        Serial << band << ": " << volume[band] << 0 << " " << 0 << endl;
-      case 1:
-        strip.setPixelColor(i, volume[band], 0, 0);
-        Serial << band << ": " << volume[band] << " " << volume[band] << " 0" << endl;
-      case 2:
-        strip.setPixelColor(i, volume[band], volume[band], 0);
-        Serial << band << ": " << volume[band] << " " << volume[band] << " 0" << endl;
-      case 3:
-        strip.setPixelColor(i, volume[band]/2, volume[band], 0);
-        Serial << band << ": " << volume[band]/2 << " " << volume[band] << " 0" << endl;
-      case 4:
-        strip.setPixelColor(i, 0, volume[band], volume[band]);
-        Serial << band << ": " << "0 " << volume[band] << " " << volume[band] << endl;
-      case 5:
-        strip.setPixelColor(i, 0,0, volume[band]);
-        Serial << band << ": " << "0 " << "0 " << volume[band] << endl;
-      case 6:
-        strip.setPixelColor(i, volume[band], 0, volume[band]);
-        Serial << band << ": " << volume[band] << " 0 " << volume[band] << endl;
-    }
-  }
-  strip.show();
+void reset(){
+    digitalWrite(spectrumStrobe, LOW);
+    //delay(1);
+    digitalWrite(spectrumReset, HIGH);
+    //delay(1);
+    digitalWrite(spectrumStrobe, HIGH);
+    //delay(1);
+    digitalWrite(spectrumStrobe, LOW);
+    //delay(1);
+    digitalWrite(spectrumReset, LOW);
+    //delay(5);
+    // Reading the analyzer now will read the lowest frequency.
 }
-
-
